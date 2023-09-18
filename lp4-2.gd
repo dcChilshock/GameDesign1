4f10460f6dee9880fad2ae0880611a5399215aa2
dcChilshock <densonchilders123@gmail.com>
2023-09-18 12:50:51 -0500

    Update

---
+++ lp4-2.gd
@@ -0,0 +1,38 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_exit_pressed():
+	get_tree().quit()
+
+
+func _on_clear_pressed():
+	$weightedit.text=""
+	$widthedit.text=""
+	$heightedit.text=""
+	$lengthedit.text=""
+	$Label3.text=""
+
+
+func _on_calculate_pressed():
+	var weight = int($weightedit.text)
+	var height = int($heightedit.text)
+	var length = int($lengthedit.text)
+	var width = int($widthedit.text)
+	var size = height * length * width
+	if size < 100000 and weight < 27:
+		$Label3.text="Your package is within parameters"
+	elif size > 100000 and weight > 27:
+		$Label3.text="too heavy and too big"
+	elif size < 100000 and weight > 27:
+		$Label3.text="Too heavy" 
+	
