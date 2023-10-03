e3ff7584566d3b25c2385466fc134a4110750fe7
dcChilshock <densonchilders123@gmail.com>
2023-10-03 12:51:36 -0500

    calculate

--- prog_152b.gd
+++ prog_152b.gd
@@ -16,9 +16,11 @@ func _on_calculate_pressed():
 	var itn = 0
 	var itnp = 0
 	var sum = 0
+	var line = "%d ----- %d" % [itn, itnp]
 	$ItemList.add_item("Even interger, Sum") 
 	while itn >= nb: 
+		$ItemList.add_item(line)
 		itn + 2 
 		itnp += itn == sum
-		$ItemList.add_item(itn,sum) 
 		itnp + 2
+		
