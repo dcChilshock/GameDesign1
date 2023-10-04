88e2b65c5ad26506c2c3d2199524755ed9d39e46
dcChilshock <densonchilders123@gmail.com>
2023-10-04 13:00:54 -0500

    update
    figured out i need return in the funcs

--- calculate.gd
+++ calculate.gd
@@ -9,22 +9,44 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-var num1 = int($LineEdit.text)
-var num2 = int($LineEdit2.text)
-var div = 0
-var remain = 0
+
 func add2num(num1, num2): 
-	num1 += num2 
+	var add = num1 + num2
+	return num1 + num2
+
 func sub2num(num1, num2):
-	num1 -= num2
+	var subtract = num1 - num2
+	return num1 - num2
+
 func mul2num(num1,num2):
-	num1 *= num2
+	var multiply = num1 * num2
+	return num1 * num2
+
 func div2num(num1,num2):
-	num1 /= num2
-func pow2num(num1,num2):
-	pow(num1, num2)
+	var division = num1 / num2
+	return num1 / num2
+
+func pow2num(num1,num2): 
+	var power = pow(num1, num2)
+	return pow(num1, num2)
+
 func mod2num(num1,num2):
-	num1 /= num2 == div
+	var div = 0
+	var remain = 0
+	var leftover = 0
+	div = num1 / num2
 	floor(div)
-	num2 *= div == num2
-	num1 -= num2 == remain
+	leftover = num2 * div 
+	remain = num1 - leftover
+	return num1 - leftover
+
+func _on_btnadd_pressed():
+	var operation = ""
+	operation = "Addition"
+	var num1 = int($lineeditone.text)
+	var num2 = int($lineedittwo.text)
+	var add = add2num(num1, num2)
+	$Resulttext.text= ": %d" % [add] 
+	
+func _on_btnmod_pressed():
+	pass # Replace with function body.
