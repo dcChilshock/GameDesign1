ee9bb4afc6aaa5ff5452733c16aed02f2da15c5b
dcChilshock <densonchilders123@gmail.com>
2023-10-04 12:15:50 -0500

    update

--- calculate.gd
+++ calculate.gd
@@ -9,3 +9,22 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+var num1 = int($LineEdit.text)
+var num2 = int($LineEdit2.text)
+var div = 0
+var remain = 0
+func add2num(num1, num2): 
+	num1 += num2 
+func sub2num(num1, num2):
+	num1 -= num2
+func mul2num(num1,num2):
+	num1 *= num2
+func div2num(num1,num2):
+	num1 /= num2
+func pow2num(num1,num2):
+	pow(num1, num2)
+func mod2num(num1,num2):
+	num1 /= num2 == div
+	floor(div)
+	num2 *= div == num2
+	num1 -= num2 == remain
