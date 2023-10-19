04610b7b4e379ca9151e90c051d8ecadd509d6fb
dcChilshock <densonchilders123@gmail.com>
2023-10-19 12:52:31 -0500

    dsgfgfdhfd

--- kdbexit.gd
+++ kdbexit.gd
@@ -1,5 +1,8 @@
 extends Area2D
 
+@export var next_level = ""
 func _on_body_entered(body):
 	if body.name == "kbdplayer":
-		OS.alert("You win")
+		OS.alert("You won the level.")
+		var lvl = "res://" + next_level + ".tscn"
+		get_tree().change_scene_to_file(lvl)
