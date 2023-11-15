ea6dfee4ff6508fc0f0e0fb27e956969ce5a1679
dcChilshock <densonchilders123@gmail.com>
2023-11-14 22:12:45 -0600

    Update
    lafghgjafsh

---
+++ TDLORETEXT.gd
@@ -0,0 +1,18 @@
+extends Node
+ 
+
+# Flowers only grow amoung the dead (aka dead bodys, graves, skeletons etc) will be explained in text to player
+
+#Player is looking for evil magic to revive loved one. Goes through trials to get the magic (either necromancy, demons,
+#or eldricht horror. Goes horribly wrong, and player has to fight loved one +whatever they summoned 
+
+#will be choices so you can either procced or doubt yourself. 
+
+#ENDING 1  Player goes through with starting plan. fight boss+loved ones
+
+#ENDING 2 Player doesnt go through with plan ends there. having finally made peace with their deaths
+
+#ENDING 3 Player gets stopped by someone if you tell them about your plan. Can either kill them or listen to them.
+#end3 game continues with older player trying to go through with his plan only now even more graves. 
+#VERY HARD but player should be able to beat the game. 2 paths for end 3. 1. same as end 1 but older player. 
+#2. Dies to the swarm 
